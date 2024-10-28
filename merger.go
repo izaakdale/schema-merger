5c9e5d9c89c30e59846cc147a945a0367558ade8
Izaak Dale <izaakdaledev@gmail.com>
2024-10-28 01:07:02 -0700

    add required merging

--- merger.go
+++ merger.go
@@ -11,16 +11,19 @@ import (
 )
 
 type schema struct {
-	Type       string         `json:"type"`
-	AllOf      []ref          `json:"allOf"`
-	Properties map[string]any `json:"properties"`
+	Type       string         `json:"type,omitempty"`
+	Schema     string         `json:"$schema,omitempty"`
+	AllOf      []subschema    `json:"allOf,omitempty"`
+	Properties map[string]any `json:"properties,omitempty"`
+	Required   []string       `json:"required,omitempty"`
 }
 
-type ref struct {
+type subschema struct {
 	Ref string `json:"$ref"`
 }
 
 func main() {
+
 	args := os.Args
 
 	if len(args) < 3 || len(args) > 3 {
@@ -35,7 +38,7 @@ func main() {
 	if err := json.Unmarshal(body, &s); err != nil {
 		panic(err)
 	}
-	if err := includeRefs(s.AllOf, s); err != nil {
+	if err := includeRefs(s.AllOf, &s); err != nil {
 		panic(err)
 	}
 	// all refs have been recursively included so we can remove the root refs.
@@ -51,39 +54,40 @@ func main() {
 	}
 }
 
-func includeRefs(refs []ref, s schema) error {
-	for _, ref := range refs {
-		var refBuff bytes.Buffer
-		if strings.HasPrefix(ref.Ref, "http") {
-			resp, err := http.Get(ref.Ref)
+func includeRefs(subs []subschema, s *schema) error {
+	for _, sub := range subs {
+		var subschemaBuff bytes.Buffer
+		if strings.HasPrefix(sub.Ref, "http") {
+			resp, err := http.Get(sub.Ref)
 			if err != nil {
 				return err
 			}
 			defer resp.Body.Close()
-			_, err = refBuff.ReadFrom(resp.Body)
+			_, err = subschemaBuff.ReadFrom(resp.Body)
 			if err != nil {
 				return err
 			}
 		} else {
-			f, err := os.Open(ref.Ref)
+			f, err := os.Open(sub.Ref)
 			if err != nil {
 				return err
 			}
 			defer f.Close()
-			_, err = refBuff.ReadFrom(f)
+			_, err = subschemaBuff.ReadFrom(f)
 			if err != nil {
 				return err
 			}
 		}
 
 		var temp schema
-		if err := json.Unmarshal(refBuff.Bytes(), &temp); err != nil {
+		if err := json.Unmarshal(subschemaBuff.Bytes(), &temp); err != nil {
 			return err
 		}
 		if temp.AllOf != nil {
-			includeRefs(temp.AllOf, temp)
+			includeRefs(temp.AllOf, &temp)
 		}
 		maps.Copy(s.Properties, temp.Properties)
+		s.Required = append(s.Required, temp.Required...)
 	}
 	return nil
 }
